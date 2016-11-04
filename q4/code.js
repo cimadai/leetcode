01a7eab9339a655ad5baf661e244d4d21ea8dc39
SHIMADA, Daisuke <dice.k1984@gmail.com>
2016-11-04 13:44:24 +0900

    Update js code

--- q4/code.js
+++ q4/code.js
@@ -7,72 +7,24 @@ var findMedianSortedArrays = function(nums1, nums2) {
     var total = nums1.length + nums2.length;
     var isOdd = total & 1;
     var med0, med1;
-    if (!isOdd) {
+    if (isOdd) {
+        med0 = (total/2)|0;
+    } else {
         med1 = (total/2)|0;
         med0 = med1 - 1;
-    } else {
-        med0 = (total/2)|0;
     }
 
-    var idx1 = 0, idx2 = 0;
-    var cnt = 0;
-    var sum = 0;
-    do {
-        var n1 = (nums1[idx1] + 1)|0;
-        var n2 = (nums2[idx2] + 1)|0;
-        if (n1 == 0 && n2 == 0) { break; }
+    var idx1 = 0, idx2 = 0, sum = 0;
+    for (var i = 0, l = isOdd ? med0 : med1; i < l + 1; ++i) {
+        var n1 = (nums1[idx1] + 1)|0,
+            n2 = (nums2[idx2] + 1)|0,
+            n  = n1 == 0 ? (++idx2, n2) : n2 == 0 ? (++idx1, n1) : n1 < n2 ? (++idx1, n1) : (++idx2, n2);
 
-        if (n1 == 0) {
-            if (med0 == cnt) {
-                sum += n2;
-                if (isOdd) { break; }
-            }
-            if (med1 == cnt) {
-                sum += n2;
-                break;
-            }
-            ++cnt;
-            ++idx2;
-        } else if (n2 == 0) {
-            if (med0 == cnt) {
-                sum += n1;
-                if (isOdd) { break; }
-            }
-            if (med1 == cnt) {
-                sum += n1;
-                break;
-            }
-            ++cnt;
-            ++idx1;
-        } else {
-            if (n1 < n2) {
-                if (med0 == cnt) {
-                    sum += n1;
-                    if (isOdd) { break; }
-                }
-                if (med1 == cnt) {
-                    sum += n1;
-                    break;
-                }
-                ++cnt;
-                ++idx1;
-            } else {
-                if (med0 == cnt) {
-                    sum += n2;
-                    if (isOdd) { break; }
-                }
-                if (med1 == cnt) {
-                    sum += n2;
-                    break;
-                }
-                ++cnt;
-                ++idx2;
-            }
+        if (med0 == i || med1 == i) {
+            sum += n;
         }
-    } while(1);
+    }
+
     return isOdd ? sum - 1 : (sum - 2) / 2;
 };
-
-
-
 
