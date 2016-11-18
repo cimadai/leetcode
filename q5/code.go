7ae356a0fbfc09d61c924d38af38aa5115d8f1ca
SHIMADA, Daisuke <dice.k1984@gmail.com>
2016-11-18 20:46:02 +0900

    Speedup q5

--- q5/code.go
+++ q5/code.go
@@ -9,6 +9,19 @@ func getLongestMatchLength(r1 []rune, r2 []rune) int {
 	}
 	return match
 }
+func getLongestMatchLength2(r []rune, halfLen int, midLen int) (int, int) {
+	match := 0
+	for i1, i2 := halfLen - 1, halfLen+midLen; i1 >= 0; i1, i2, match = i1-1, i2+1, match + 1 {
+		if r[i1] != r[i2] {
+			break
+        }
+	}
+    if match > 0 {
+        return match, midLen + match*2
+    } else {
+		return 0, 0
+    }
+}
 
 func longestPalindrome(s string) string {
 	runes := []rune(s)
@@ -23,46 +36,47 @@ func longestPalindrome(s string) string {
 	end := 0
 	for i := 1; i < leng - 1; i++ {
 		isOdd := (i & 1) == 1
+        var midLen int // 中心部の長さ
 		var leftLen int
 		var rightLen int
-		var rightFrom int
+		//var rightFrom int
 		if isOdd {
+			midLen = 0
 			leftLen = (i + 1) / 2
-			rightFrom = leftLen
+			//rightFrom = leftLen
 			rightLen = original - leftLen
 		} else {
+			midLen = 1
 			leftLen = i / 2
-			rightFrom = leftLen + 1
+			//rightFrom = leftLen + 1
 			rightLen = original - leftLen - 1
 		}
+
+        // 左右の長さの短い方
 		var minimumLen int
 		if leftLen > rightLen {
 			minimumLen = rightLen
 		} else {
 			minimumLen = leftLen
 		}
-		probableLongest := rightFrom - leftLen + minimumLen * 2
+
+        // 最大ここまでの長さの回文が得られそう
+		probableLongest := midLen + minimumLen * 2
 
 		if longestLen >= probableLongest {
 			// すでに知ってる長さよりも短いので確かめる必要が無い
 			continue
 		}
 
-		//fmt.Printf("i = %d, leftLen = %d, rightLen = %d, rightFrom = %d, minimumLen = %d\n", i, leftLen, rightLen, rightFrom, minimumLen)
-		r1 := runes[leftLen - minimumLen : leftLen]
-		r2 := runes[rightFrom : rightFrom + minimumLen]
-		longestMatch := getLongestMatchLength(r1, r2)
-		f := leftLen - longestMatch
-		e := rightFrom + longestMatch
-		longest := e - f
-
+        begin := leftLen - minimumLen
+		r := runes[begin : begin + probableLongest]
+        match, longest := getLongestMatchLength2(r, minimumLen, midLen)
 		if longestLen < longest {
 			// すでに知ってる長さよりも長いので改める
 			longestLen = longest
-			from = f
-			end = e
+			from = leftLen - match
+			end = from + longest
 		}
-		//fmt.Printf("longestLen = %d, r1 = %s, r2 = %s\n", longestLen, string(r1), string(r2))
 	}
 	return string(runes[from:end])
 }
