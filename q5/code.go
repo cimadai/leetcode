5d1cf9c864769b54a8b24a3f16e20ed9ee83b7fb
SHIMADA, Daisuke <dice.k1984@gmail.com>
2016-11-30 18:41:16 +0900

    Fix code indent.

--- q5/code.go
+++ q5/code.go
@@ -14,13 +14,13 @@ func getLongestMatchLength2(r []rune, halfLen int, midLen int) (int, int) {
 	for i1, i2 := halfLen - 1, halfLen+midLen; i1 >= 0; i1, i2, match = i1-1, i2+1, match + 1 {
 		if r[i1] != r[i2] {
 			break
-        }
+		}
 	}
-    if match > 0 {
-        return match, midLen + match*2
-    } else {
+	if match > 0 {
+		return match, midLen + match*2
+	} else {
 		return 0, 0
-    }
+	}
 }
 
 func longestPalindrome(s string) string {
@@ -36,23 +36,20 @@ func longestPalindrome(s string) string {
 	end := 0
 	for i := 1; i < leng - 1; i++ {
 		isOdd := (i & 1) == 1
-        var midLen int // 中心部の長さ
+		var midLen int // 中心部の長さ
 		var leftLen int
 		var rightLen int
-		//var rightFrom int
 		if isOdd {
 			midLen = 0
 			leftLen = (i + 1) / 2
-			//rightFrom = leftLen
 			rightLen = original - leftLen
 		} else {
 			midLen = 1
 			leftLen = i / 2
-			//rightFrom = leftLen + 1
 			rightLen = original - leftLen - 1
 		}
 
-        // 左右の長さの短い方
+		// 左右の長さの短い方
 		var minimumLen int
 		if leftLen > rightLen {
 			minimumLen = rightLen
@@ -60,7 +57,7 @@ func longestPalindrome(s string) string {
 			minimumLen = leftLen
 		}
 
-        // 最大ここまでの長さの回文が得られそう
+		// 最大ここまでの長さの回文が得られそう
 		probableLongest := midLen + minimumLen * 2
 
 		if longestLen >= probableLongest {
@@ -68,9 +65,9 @@ func longestPalindrome(s string) string {
 			continue
 		}
 
-        begin := leftLen - minimumLen
+		begin := leftLen - minimumLen
 		r := runes[begin : begin + probableLongest]
-        match, longest := getLongestMatchLength2(r, minimumLen, midLen)
+		match, longest := getLongestMatchLength2(r, minimumLen, midLen)
 		if longestLen < longest {
 			// すでに知ってる長さよりも長いので改める
 			longestLen = longest
